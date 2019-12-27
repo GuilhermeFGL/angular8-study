b26eb60cacdf0196c8a9cc13d1382c8242cf76e7
Guilherme Faria <guilherme_fgl@hotmail.com>
2019-12-27 15:34:18 -0300

    string interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
+
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PropertyBindingComponent
+    PropertyBindingComponent,
+    StringInterpolationComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
