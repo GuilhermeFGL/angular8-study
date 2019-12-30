9b318649a1fb1ab7c413520235df8a7666b04694
Guilherme Faria <guilherme_fgl@hotmail.com>
2019-12-30 15:21:42 -0300

    ng content dicrective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { NgForComponent } from './ng-for/ng-for.component';
 import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 import { NgTemplateComponent } from './ng-template/ng-template.component';
 import { NgContainerComponent } from './ng-container/ng-container.component';
+import { NgContentComponent } from './ng-content/ng-content.component';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,8 @@ import { NgContainerComponent } from './ng-container/ng-container.component';
     NgForComponent,
     NgSwitchComponent,
     NgTemplateComponent,
-    NgContainerComponent
+    NgContainerComponent,
+    NgContentComponent
   ],
   imports: [
     FormsModule,
