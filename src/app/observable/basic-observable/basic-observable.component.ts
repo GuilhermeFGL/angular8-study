e9366aaf4034b9dfdbf4f8111ecad2a1af2507b3
Guilherme Faria <guilherme_fgl@hotmail.com>
2020-01-29 10:07:16 -0300

    cold observable

--- src/app/observable/basic-observable/basic-observable.component.ts
+++ src/app/observable/basic-observable/basic-observable.component.ts
@@ -10,6 +10,8 @@ export class BasicObservableComponent implements OnInit {
 
   output: string[] = [];
   asynchronousSubscription: Subscription;
+  coldObservableSubscription1: Subscription;
+  coldObservableSubscription2: Subscription;
 
   constructor() { }
 
@@ -20,6 +22,12 @@ export class BasicObservableComponent implements OnInit {
     this.output = [];
     if (this.asynchronousSubscription) {
       this.asynchronousSubscription.unsubscribe();
+    }
+    if (this.coldObservableSubscription1) {
+      this.coldObservableSubscription1.unsubscribe();
+    }
+    if (this.coldObservableSubscription2) {
+      this.coldObservableSubscription2.unsubscribe();
     }
   }
 
@@ -42,7 +50,7 @@ export class BasicObservableComponent implements OnInit {
 
   asynchronous() {
     this.clear();
-    
+
     const observable = new Observable(
       (observer: Observer<any>) => {
         let i: number = 0;
@@ -65,5 +73,43 @@ export class BasicObservableComponent implements OnInit {
       (n) => { },
       (error) => { this.output.push(error) },
       () => { this.output.push('Complete') });
+  }
+
+  coldObservable() {
+    this.clear();
+
+    const observable = new Observable(
+      (observer: Observer<any>) => {
+        let i: number = 0;
+        let id = setInterval(() => {
+          i++;
+          this.output.push(`From observable: ${i}`)
+          if (i == 10) {
+            observer.complete();
+          } else if (i % 2 == 0) {
+            observer.next(1);
+          }
+        }, 500);
+        return () => {
+          clearInterval(id);
+        };
+      }
+    );
+
+    this.output.push('Cold Observable 1');
+    this.coldObservableSubscription1 = observable.subscribe(
+      (n) => { },
+      (error) => { this.output.push(error) },
+      () => { this.output.push('Complete 1') });
+
+    this.output.push('Cold Observable 2');
+    let interval = setInterval(() => {
+      this.coldObservableSubscription2 = observable.subscribe(
+        (n) => { },
+        (error) => { this.output.push(error) },
+        () => { this.output.push('Complete 2') });
+      clearInterval(interval);
+    }, 300);
+
   }
 }
